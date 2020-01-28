253e2c77a1db703f3308ba0d260a1a8ff7f50768
yr <172808H@mymail.nyp.edu.sg>
2020-01-29 01:13:02 +0800

    delete tab pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+// import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -24,7 +24,7 @@ import { ShoppingMallSearchingRoutingArcgisPage } from '../pages/shopping-mall-s
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
+    // TabsPage,
     CarparkHeatmapBufferingGooglemapPage,
     IndoorNavigationMapwizePage,
     ShoppingMallSearchingRoutingArcgisPage
@@ -40,7 +40,7 @@ import { ShoppingMallSearchingRoutingArcgisPage } from '../pages/shopping-mall-s
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
+    // TabsPage,
     CarparkHeatmapBufferingGooglemapPage,
     IndoorNavigationMapwizePage,
     ShoppingMallSearchingRoutingArcgisPage
