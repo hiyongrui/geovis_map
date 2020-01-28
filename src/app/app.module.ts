5931208322a5aad77a5fb27e5610c45e22250f58
yr <172808H@mymail.nyp.edu.sg>
2020-01-29 01:14:32 +0800

    delete initial tab of 3 pages - home, about, contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-// import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Geolocation } from '@ionic-native/geolocation';
@@ -21,10 +16,6 @@ import { ShoppingMallSearchingRoutingArcgisPage } from '../pages/shopping-mall-s
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    // TabsPage,
     CarparkHeatmapBufferingGooglemapPage,
     IndoorNavigationMapwizePage,
     ShoppingMallSearchingRoutingArcgisPage
@@ -37,10 +28,6 @@ import { ShoppingMallSearchingRoutingArcgisPage } from '../pages/shopping-mall-s
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    // TabsPage,
     CarparkHeatmapBufferingGooglemapPage,
     IndoorNavigationMapwizePage,
     ShoppingMallSearchingRoutingArcgisPage
